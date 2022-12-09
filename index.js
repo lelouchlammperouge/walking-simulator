c9674ec4ae78722ae46b492bddb74d0ed1642612
lelouch <93632866+lelouchlammperouge@users.noreply.github.com>
2022-12-09 11:23:17 -0700

    player1 game update

--- index.js
+++ index.js
@@ -8,9 +8,94 @@ canvas.height = 579;
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const playerImage = new Image();
+playerImage.src = "./img/playerDown (1).png";
+
 const image = new Image();
 image.src = "./map/town.png";
 
-image.onload = () => {
-  c.drawImage(image, 0, 0);
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position;
+    this.image = image;
+  }
+
+  draw() {
+    c.drawImage(this.image, -700, -110);
+  }
+}
+
+const background = new Sprite({
+  position: {
+    x: -700,
+    y: -110,
+  },
+  image: image,
+});
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
 };
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  background.draw();
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    695,
+    500,
+    playerImage.width / 4,
+    playerImage.height
+  );
+}
+
+animate();
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "s":
+      keys.s.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});

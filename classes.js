91f133e6a17e42adf051deba9d0fc9b988e924fa
lelouch <93632866+lelouchlammperouge@users.noreply.github.com>
2023-01-17 10:11:20 -0700

    walking

--- classes.js
+++ classes.js
@@ -2,7 +2,7 @@ class Sprite {
   constructor({ position, velocity, image, frames = { max: 1 } }) {
     this.position = position;
     this.image = image;
-    this.frames = frames;
+    this.frames = { ...frames, val: 0 };
     this.image.onload = () => {
       this.width = this.image.width / this.frames.max;
       this.height = this.image.height;
@@ -12,7 +12,7 @@ class Sprite {
   draw() {
     c.drawImage(
       this.image,
-      0,
+      this.frames.val * this.width,
       0,
       this.image.width / this.frames.max,
       this.image.height,
@@ -21,6 +21,8 @@ class Sprite {
       this.image.width / this.frames.max,
       this.image.height
     );
+    if (this.frames.val < this.frames.max) this.frames.val++;
+    else this.frames.val = 0;
   }
 }
 
@@ -33,7 +35,7 @@ class Boundary {
     this.height = 48;
   }
   draw() {
-    c.fillStyle = "red";
+    c.fillStyle = "rgba(255, 0, 0, 0)";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
